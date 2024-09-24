db438730b3348a2081d1d4d46396f5d6e15c4ccb
Amato Luca <amatoroccoluca@gmail.com>
2024-09-24 16:29:56 +0200

    chore: add useImperativeHandle hook to ResultModal component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,17 @@
 import React, { useRef, useState } from 'react';
+import ResultModal from './ResultModal';
 
 const TimerChallenge = ({ title, targetTime }) => {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   const timer = useRef();
+  const dialog = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -19,21 +22,23 @@ const TimerChallenge = ({ title, targetTime }) => {
   }
 
   return (
-    <div className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running' : 'Timer inactive'}
-      </p>
-    </div>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <div className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running' : 'Timer inactive'}
+        </p>
+      </div>
+    </>
   );
 };
 
